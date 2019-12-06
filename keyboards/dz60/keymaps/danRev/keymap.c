5a9fb299dde4263ff6ef3a8a7ea97e6ebf3a62de
hummingbird1986 <39380985+hummingbird1986@users.noreply.github.com>
2019-12-06 16:43:05 +0800

    Edit the HHKB verison. Move FN to right command

--- keyboards/dz60/keymaps/danRev/keymap.c
+++ keyboards/dz60/keymaps/danRev/keymap.c
@@ -16,8 +16,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_GRV,          KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS, KC_EQL,  KC_NO,KC_BSLS,
                  LT(_VIML,KC_TAB),        KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC,KC_BSPC,
                  CTL_T(KC_ESC),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT, KC_ENT,
-                 KC_LSFT, KC_NO,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,MO(_HHKB/*|_MD*/),
-                 LT(_MCO,KC_CAPS),KC_LALT,KC_LGUI,           KC_NO,KC_SPC,KC_NO,        KC_RGUI,KC_RALT,KC_NO,KC_RCTL,KC_DEL),
+                 KC_LSFT, KC_NO,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,KC_DEL,
+                 LT(_MCO,KC_CAPS),KC_LALT,KC_LGUI,           KC_NO,KC_SPC,KC_NO,       MO(_HHKB/*|_MD*/),KC_RGUI,KC_RALT,KC_NO,KC_RCTL),
     
     [_VIML]=LAYOUT(
                    _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,_______,_______,_______,_______,_______,_______,_______,_______,_______,
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [_HHKB]=LAYOUT(
                    _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-                   _______,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,_______,_______,_______,_______,_______,  KC_UP,_______,KC_EJCT,
-                   _______, KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,_______,_______,KC_HOME,KC_PGUP,KC_LEFT,KC_RIGHT,_______,
-                   _______,_______,_______,_______,_______,_______,_______,_______,_______, KC_END,KC_PGDN,KC_DOWN,_______,_______,
+                   _______,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,_______,_______,_______,_______,  KC_UP,_______,_______,KC_EJCT,
+                   _______, KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,_______,KC_HOME,KC_PGUP,KC_LEFT,KC_RIGHT,_______,_______,
+                   _______,_______,_______,_______,_______,_______,_______,_______, KC_END,KC_PGDN,KC_DOWN,_______,_______,_______,
                    _______,_______,_______,_______,_______,_______,_______,TG(_NL),_______,_______,_______),
     
     
