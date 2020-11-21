e4f2f6b7f0cb497f7279a6ebfbb7c19e74423013
Hummingbird1986.local <danieldeng@me.com>
2020-11-21 21:22:32 +0800

    add _BRKT layer

--- users/danRev/danRev.c
+++ users/danRev/danRev.c
@@ -1,5 +1,94 @@
 #include "danRev.h"
 
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+	case _LCBRKT:
+		if(record->event.pressed){
+
+	register_code(KC_LSFT);
+	register_code(KC_LBRC);
+      } else {
+        unregister_code(KC_LSFT);
+		unregister_code(KC_LBRC);
+      }
+	  break;
+
+	case _RCBRKT:
+	   if (record->event.pressed) {
+	register_code(KC_RSFT);
+	register_code(KC_RBRC);
+      } else {
+        unregister_code(KC_RSFT);
+		unregister_code(KC_RBRC);
+      }
+	  break;
+
+	case _LBRKT:
+	   if (record->event.pressed){
+		 register_code(KC_LBRC);
+	   }
+	   else{
+		 unregister_code(KC_LBRC);
+	   }
+	   break;
+
+	case _RBRKT:
+	   if (record->event.pressed){
+		 register_code(KC_RBRC);
+	   }
+	   else{
+		 unregister_code(KC_RBRC);
+	   }
+	   break;
+
+
+    case _MINS:
+	   if (record->event.pressed) {
+	    register_code(KC_MINS);
+	 	 
+      } else {
+        
+		unregister_code(KC_MINS);
+      }
+	  break;
+	  
+    case _EQL:
+	   if (record->event.pressed) {
+	    register_code(KC_EQL);
+	 	 
+      } else {
+        
+		unregister_code(KC_EQL);
+      }
+	  break;
+//code not working
+/*	case _TEST:
+	  if (record->event.pressed){
+		switch(keycode){
+           case KC_RSFT:
+			if(record->event.pressed)
+			{
+			  register_code(KC_5);
+			  unregister_code(KC_5);
+			} else{
+			  register_code(KC_6);
+			  unregister_code(KC_6);
+			}break;
+		
+		}
+	  }*/
+
+  }
+  //return false;  // Skip all further processing of this key 
+ return true;
+};
+
+
+
+
+
+//old fashine 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
     switch(id) {
         case 0: {
--- users/danRev/danRev.h
+++ users/danRev/danRev.h
@@ -5,7 +5,31 @@
 #include "quantum.h"
 #include "action.h"
 #include "version.h"
+  enum customer_keycodes {
+    _BL = 0,
+    _NORM,
+    _VIML,
+    _NL,
+    _HHKB,
+    _MCO,
+    _BRKT,
+/*    _LBRC,
+    _RBRC,
+	_MINS,
+	_EQL*/
+  };
 
+  enum customer_key{
+
+    _LCBRKT=SAFE_RANGE,
+    _RCBRKT,
+	_LBRKT,
+	_RBRKT,
+	_MINS,
+	_EQL,
+    _TEST,
+  };
+bool process_record_user(uint16_t keycode, keyrecord_t *record);
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt);
 
 #endif
